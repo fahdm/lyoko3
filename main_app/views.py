7e251a6c7f86a3509f5330d6187c066fffb2e504
Fahad Mubiru <fahadmubiru1@gmail.com>
2024-05-02 13:07:40 -0700

    Handle Logic using views

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
+from .models import Post, Comment
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def post_index(request):
+    posts = Post.objects.all().order_by("-created_on")
+    context = {
+        "posts": posts,
+    }
+    return render(request, "posts/index.html", context)
+
+def post_category(request, category):
+    posts = Post.objects.filter(
+        categories__name__contains=category
+    ).order_by("-created_on")
+    context = {
+        "category": category,
+        "posts": posts,
+    }
+    return render(request, "posts/category.html", context)
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    comments = Comment.objects.filter(post=post)
+    context = {
+        "post": post,
+        "comments": comments,
+    }
+
+    return render(request, "posts/detail.html", context)
